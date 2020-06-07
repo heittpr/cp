3eec56a2e35d7486d6d291cddc2085f179f4ce9a
heitor <heitorleite.dev@gmail.com>
2020-06-07 01:25:53 -0300

    add fechadura solution

---
+++ neps/cursos/fechadura/fechadura.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M, resultado;
+bool compara(int a, int b) { return a > b; }
+
+int main() {
+  cin >> N >> M;
+  int pinos[N];
+
+  for (int i = 0; i < N; i++) cin >> pinos[i];
+
+  for (int i = 0; i < N-1; i++) {
+    int d = M - pinos[i];
+
+    pinos[i] += d;
+    pinos[i+1] += d;
+
+    resultado += abs(d);
+  }
+
+  cout << resultado << endl;
+
+  return 0;
+}
