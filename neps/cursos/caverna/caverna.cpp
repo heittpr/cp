b1fef5991042499956234a8d454005726c2813a7
heitor <heitorleite.dev@gmail.com>
2020-06-07 13:32:35 -0300

    add caverna de ordinskaya solution

---
+++ neps/cursos/caverna/caverna.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+long long N, M, resposta, ultimo=0, a;
+
+int main() {
+  cin >> N >> M;
+
+  for (int i = 0; i < N; i++) {
+    cin >> a;
+    long long mx = max(a, M-a), mn = M-mx;
+
+    if (mn >= ultimo) {
+      resposta += mn;
+      ultimo = mn;
+    } else if (mx >= ultimo) {
+      resposta += mx;
+      ultimo = mx;
+    } else {
+      cout << -1 << endl;
+      return 0;
+    }
+  }
+
+  cout << resposta << endl;
+  return 0;
+}
