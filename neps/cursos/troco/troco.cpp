304e675fe38498e2cbbc3446b426b0afdef161be
heitor <heitorleite.dev@gmail.com>
2020-06-07 12:49:42 -0300

    add troco fácil solution

---
+++ neps/cursos/troco/troco.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int moedas[6] = { 100, 50, 25, 10, 5, 1 };
+int N, resposta;
+
+int main() {
+  cin >> N;
+
+  for ( int moeda : moedas ) {
+    resposta += N / moeda;
+    N = N % moeda;
+
+    if ( N == 0 ) break;
+  }
+  
+  cout << resposta << endl;
+  return 0;
+}
