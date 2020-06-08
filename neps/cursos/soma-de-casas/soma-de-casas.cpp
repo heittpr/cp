d1602dadabb3b67c2dac1c7fa77bb2d1d59129b8
heitor <heitorleite.dev@gmail.com>
2020-06-08 14:28:11 -0300

    add soma de casas solution

---
+++ neps/cursos/soma-de-casas/soma-de-casas.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+#define MAXN 100000
+int N, K, casas[MAXN];
+
+int busca(int x) {
+  int ini=0, fim=N-1, meio;
+
+  while (ini<=fim) {
+    meio = (ini+fim)/2;
+
+    if (casas[meio] > x) fim = meio-1;
+    if (casas[meio] < x) ini = meio+1;
+    if (casas[meio] == x) return true;
+  }
+
+  return false;
+}
+
+int main() {
+  cin >> N;
+  for (int i = 0; i < N; i++) cin >> casas[i];
+  cin >> K;
+
+  for (int i = 0; i < N; i++) {
+    if (busca(K-casas[i])) {
+      cout << casas[i] << ' ' << K-casas[i] << endl;
+      break;
+    }
+  }
+
+  return 0;
+}
