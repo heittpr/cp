4ec19534c5327c966a43d7ded7be4a50a52b19ad
heitor <heitorleite.dev@gmail.com>
2020-10-12 00:54:11 -0300

    add nota esquecida solution

---
+++ obi/2019/2/nota.cpp
@@ -0,0 +1,15 @@
+// Nota esquecida
+
+#include <iostream>
+using namespace std;
+#define endl "\n"
+
+int a, m;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> a >> m;
+  cout << 2*m -a << endl;
+}
