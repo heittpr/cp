492affbb5a76afdb04337723bb95f69005b605ca
heitor <heitorleite.dev@gmail.com>
2020-10-29 01:20:46 -0300

    add jogo da memória solution

--- neps/cursos/grafos/intermediarios/lca/jogo.cpp
+++ neps/cursos/grafos/intermediarios/lca/jogo.cpp
@@ -1,43 +1,44 @@
 // Jogo da Memória
 // https://neps.academy/lesson/219
 
+#include <algorithm>
 #include <iostream>
-#include <vector>
 #include <cstring>
+#include <vector>
 
 using namespace std;
-typedef vector<int> vi;
-#define endl "\n"
+typedef long long ll;
 #define pb push_back
-#define MAXN 50002
-#define MAXL 16
+#define endl "\n"
+#define maxn 50010
+#define maxh 20
 
-int n, c[MAXN], par[MAXN];
-int pai[MAXN],  lvl[MAXN];
-int ancestral[MAXN][MAXL];
-vi adj[MAXN];
+int ancestral[maxn][maxh];
+int pai[maxn], par[maxn];
+int n, h[maxn], c[maxn];
+vector<int> adj[maxn];
 
-void dfs(int u) {
-  for (int v : adj[u]) {
-    if (lvl[v] == -1) {
-      lvl[v] = lvl[u] + 1;
-      pai[v] = u;
+void dfs(int v) {
+  for (int u : adj[v]) {
+    if (h[u] == -1) {
+      h[u] = h[v]+1;
+      pai[u] = v;
 
-      dfs(v);
+      dfs(u);
     }
   }
 }
 
 int lca(int u, int v) {
-  if (lvl[u] < lvl[v]) swap(u, v);
+  if (h[u] < h[v]) swap(u, v);
 
-  for (int i = MAXL-1; i >= 0; i--)
-    if (lvl[u] - (1<<i) >= lvl[v])
+  for (int i = maxh-1; i >= 0; i--)
+    if (h[u] - (1<<i) >= h[v])
       u = ancestral[u][i];
 
   if (u == v) return u;
 
-  for (int i = MAXL-1; i >= 0; i--)
+  for (int i = maxh-1; i >= 0; i--)
     if (ancestral[u][i] != -1 && ancestral[u][i] != ancestral[v][i]) {
       u = ancestral[u][i];
       v = ancestral[v][i];
@@ -56,41 +57,39 @@ int main() {
   for (int i = 1; i <= n; i++) {
     cin >> tmp;
 
-    if (c[tmp]) {
+    if(c[tmp]) {
       par[i] = c[tmp];
       par[c[tmp]] = i;
     }
 
     c[tmp] = i;
   }
-  
+
   int a, b;
   for (int i = 0; i < n-1; i++) {
     cin >> a >> b;
-    
+
     adj[a].pb(b);
     adj[b].pb(a);
   }
 
-  memset(lvl, -1, sizeof lvl);
   memset(pai, -1, sizeof pai);
+  memset(h, -1, sizeof h);
   memset(ancestral, -1, sizeof ancestral);
 
-  lvl[1] = 0;
-  dfs(1);
+  h[1] = 0; dfs(1);
 
   for (int i = 1; i <= n; i++)
     ancestral[i][0] = pai[i];
 
-  for (int j = 1; j < MAXL; j++)
+  for (int j = 1; j < maxh; j++)
     for (int i = 1; i <= n; i++)
       if (ancestral[i][j-1] != -1)
         ancestral[i][j] = ancestral[ancestral[i][j-1]][j-1];
 
-  long long res = 0LL;
-
-  for(int i = 1;i <= n;i++)
-    res += (long long)(lvl[i] + lvl[par[i]] - 2*lvl[lca(i, par[i])]);
+  ll res = 0LL;
+  for (int i = 1; i <= n; i++)
+    res += (ll)(h[i] + h[par[i]] - 2 * h[lca(i, par[i])]);
 
   cout << res/2 << endl;
 }
