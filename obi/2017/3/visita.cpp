7bc3e51c7c0f971e87d08316f15fefea56362737
heitor <heitorleite.dev@gmail.com>
2020-11-22 04:08:28 -0300

    add better visita entre as cidades solution

--- obi/2017/3/visita.cpp
+++ obi/2017/3/visita.cpp
@@ -13,31 +13,18 @@ const int maxn = 1e4+1;
 
 int n, a, b;
 vector<ii> adj[maxn];
-int dist[maxn];
-bool seen[maxn];
 
-int dijkstra() {
-  memset(dist, 0x3f, sizeof dist);
-  priority_queue<ii> q;
-  q.push({0, a});
-  dist[a] = 0;
+int dfs(int v, int p, int d) {
+  if (v == b) return d;
 
-  while (!q.empty()) {
-    int v = q.top().second;
-    q.pop();
-    
-    if (seen[v]) continue;
-    seen[v] = true;
+  for (ii n : adj[v]) {
+    int c = n.first;
+    int u = n.second;
 
-    for (ii n : adj[v]) {
-      int d = n.first;
-      int u = n.second;
-
-      if (dist[u] > dist[v] + d) {
-        dist[u] = dist[v] + d;
-        if (u == b) return dist[u];
-        q.push({-dist[u], u});
-      }
+    if (u != p) {
+      int f = dfs(u, v, d+c);
+      if (f != -1)
+        return f;
     }
   }
 
@@ -49,16 +36,14 @@ int main() {
   cin.tie(0);
   
   cin >> n >> a >> b;
-  a--, b--;
   
   int p, q, d;
   for (int i = 0; i < n-1; i++) {
     cin >> p >> q >> d;
-    p--, q--;
     
     adj[p].pb({d, q});
     adj[q].pb({d, p});
   }
-  
-  cout << dijkstra() << endl;
+
+  cout << dfs(a, a, 0) << endl;
 }
