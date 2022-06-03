4da12ed19c8da2fc36585a887760e577a803d98b
heitor <heitorleite.dev@gmail.com>
2022-06-03 10:28:27 -0300

    add coin combinations I solution

---
+++ cses/dynamic-programming/coin1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int mod = 1e9+7;
+int n, x, dp[(int)1e6+5];
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  cin >> n >> x; vector<int> c(n);
+  for (int &i : c) cin >> i;
+  dp[0] = 1;
+  for (int i = 1; i <= x; i++)
+    for (int j : c) if (j <= i)
+      (dp[i] += dp[i-j]) %= mod;
+  cout << dp[x] << '\n';
+}
