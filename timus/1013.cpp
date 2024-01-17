e05d7c326959bf38d4dd032490c20cb5abccdc82
heitor <heitorleite.dev@gmail.com>
2024-01-17 01:46:24 -0300

    update 1013 solution

--- timus/1013.cpp
+++ timus/1013.cpp
@@ -20,22 +20,17 @@ template<class T, int N> struct Matrix {
       rep(k,0,N) (a.d[i][j] += d[i][k]*m.d[k][j]) %= mod;
     return a;
   }
-  vector<T> operator*(const vector<T>& vec) const {
-    vector<T> ret(N);
-    rep(i,0,N) rep(j,0,N) (ret[i] += d[i][j] * vec[j]) %= mod;
-      return ret;
+  M operator^(ll p) const {
+    assert(p >= 0);
+    M a, b(*this);
+    rep(i,0,N) a.d[i][i] = 1;
+    while (p) {
+      if (p&1) a = a*b;
+      b = b*b;
+      p >>= 1;
     }
-    M operator^(ll p) const {
-      assert(p >= 0);
-      M a, b(*this);
-      rep(i,0,N) a.d[i][i] = 1;
-      while (p) {
-        if (p&1) a = a*b;
-        b = b*b;
-        p >>= 1;
-      }
-      return a;
-    }
+    return a;
+  }
 };
 
 int32_t main() {
@@ -43,6 +38,6 @@ int32_t main() {
   ll n, k; cin >> n >> k >> mod;
   Matrix<i128, 3> x;
   x.d = {{{k-1, k-1}, {1, 0}}};
-  i128 ans = ((x^(n-1))*(vector<i128>{k-1, 1}))[0];
-  cout << ((ll)(ans%mod)) << '\n';
+  i128 ans = (x^n).d[0][0];
+  cout << (ll)(ans%mod) << '\n';
 }
